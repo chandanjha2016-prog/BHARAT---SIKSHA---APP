9e4145da649dc8b16eb7ebd4ef8493263a8cbcc6
chandanjha2016-prog <chandanjha2016@gmail.com>
2026-07-31 11:58:33 +0530

    Add Footer component with founder information

--- src/components/Disclaimer.jsx
+++ src/components/Disclaimer.jsx
@@ -1,1 +1,9 @@
 
+export default function Footer() {
+  return (
+    <footer className="bg-gray-800 text-white text-center p-6 mt-10">
+      <p><b>Founder:</b> Chandan Kumar | <b>Patna, Bihar</b></p>
+      <p className="mt-2">"Shiksha hi Shakti hai" | Jai Hind 🙏 2026</p>
+    </footer>
+  )
+}
